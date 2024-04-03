a85c5f2e3f0cf92070b1692ad21868cf75a26052
Jakubach <jakubach@gmail.com>
2024-04-03 13:13:39 +0200

    clearing code

--- simple_gesture.py
+++ simple_gesture.py
@@ -18,33 +18,23 @@ loop_rate = 50 #[Hz]
 imu = IMU()
 imu.gyro_data_rate = Rate.RATE_104_HZ
 imu.accelerometer_data_rate = Rate.RATE_104_HZ
-
    
-window_size = 1.5 * loop_rate #[s] * [1/s]
-data_buffer = np.zeros(int(window_size))
+window_time_size = 1.5 # [s]
+window_samples_size = window_time_size * loop_rate # [s] * [1/s]
+data_buffer = np.zeros(int(window_samples_size))
 data_iter = 0
 imu.high_pass_filter = True
-previous_value = 0
+previous_value = np.linalg.norm(imu.acceleration) 
 
 while True:
     current_msecs = supervisor.ticks_ms()
     elapsed_time = (current_msecs - previous_msecs)/1000
     if(elapsed_time >= (1.0/loop_rate)):
-        #print("%.2f \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f" % (imu.acceleration + imu.gyro))
-        #data_buffer[data_iter] = abs(imu.acceleration[0]) + abs(imu.acceleration[1]) + abs(imu.acceleration[2])#np.linalg.norm(imu.acceleration)
         data_buffer[data_iter] = np.linalg.norm(imu.acceleration)
-        #print(np.linalg.norm(np.array([-10,1])))
-        #print(imu.acceleration)
-        data_iter = (data_iter + 1)%(int(window_size))
-        #print((data_buffer[-1],data_buffer[-1]))
-        #print(data_iter,int(window_size))
-        if(data_iter == int(window_size)-1):
-            #print((rms(data_buffer),rms(data_buffer)))
+        data_iter = (data_iter + 1)%(int(window_samples_size))
+        if(data_iter == int(window_samples_size)-1):
             led.value = not led.value
-            #print(rms(data_buffer),previous_value)
-            
             accel_difference = np.max([0,rms(data_buffer) - previous_value])
-            print(accel_difference)
+            print((accel_difference,accel_difference))
             previous_value = rms(data_buffer)
-            #led_status = not led_status
         previous_msecs = current_msecs
