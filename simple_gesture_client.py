0b09003e5c6fc849e63a964eb57a4e2e9fb3f36b
Jakubach <jakubach@gmail.com>
2024-04-27 15:53:19 +0200

    Updated battery managment

--- simple_gesture_client.py
+++ simple_gesture_client.py
@@ -36,8 +36,7 @@ class StateMachine(object):
     def __init__(self):
         self.state = None
         self.states = {}
-
-
+        self._previous_state = None
 
     def add_state(self, state):
         self.states[state.name] = state
@@ -45,42 +44,55 @@ class StateMachine(object):
     def go_to_state(self, state_name):
         if self.state:
             print('Exiting %s' % (self.state.name))
+            self._previous_state = self.state.name
             self.state.exit(self)
         self.state = self.states[state_name]
         print('Entering %s' % (self.state.name))
         self.state.enter(self)
-
+        
+    def get_previous_state(self):
+        return self._previous_state
+        
     def update(self):
         if self.state:
             self.state.update(self)
 
+
+        
 class IdleState(State):
-
+    loop_rate = 50 #[Hz]
+    led_red = digitalio.DigitalInOut(board.LED_RED)
+    led_green = digitalio.DigitalInOut(board.LED_GREEN)
+    imu = IMU()
+    window_time_size = 1 # [s]
+    window_samples_size = window_time_size * loop_rate # [s] * [1/s]
+    data_buffer = np.zeros(int(window_samples_size))
+    accel_diff_threshold = 0.4 # [m/s^2]
+    data_iter = 0
+    previous_value = np.linalg.norm(imu.acceleration)
+    __previous_msecs = time.monotonic()
+    
+    bat = Battery()
+    battery_loop_rate = 10 # [Hz]
+    battery_window_time_size = 5 # [s]
+    battery_window_samples_size = battery_window_time_size * battery_loop_rate # [s] * [1/s]
+    battery_data_buffer = np.zeros(int(battery_window_samples_size))
+    battery_data_iter = 0
+    charged_battery_voltage = 4.15
+    discharged_battery_voltage = 3.5
+    
     def __init__(self):
-        self.bat = Battery()
-        self.bat.charge_current = self.bat.CHARGE_100MA
-        self.previous_msecs = time.monotonic()
-        self.loop_rate = 50 #[Hz]
-        self.imu = IMU()
-        self.imu.gyro_data_rate = Rate.RATE_104_HZ
-        self.imu.accelerometer_data_rate = Rate.RATE_104_HZ
-
-        self.window_time_size = 1.5 # [s]
-        self.window_samples_size = self.window_time_size * self.loop_rate # [s] * [1/s]
-        self.data_buffer = np.zeros(int(self.window_samples_size))
-        self.accel_diff_threshold = 0.5 # [m/s^2]
-        self.data_iter = 0
-        self.imu.high_pass_filter = True
-        self.previous_value = np.linalg.norm(self.imu.acceleration)
-        self.led_red = digitalio.DigitalInOut(board.LED_RED)
+        super().__init__()
         self.led_red.direction = digitalio.Direction.OUTPUT
         self.led_red.value = True
-        self.led_green = digitalio.DigitalInOut(board.LED_GREEN)
         self.led_green.direction = digitalio.Direction.OUTPUT
         self.led_green.value = True
-        self.charged = None
-        self.discharged_battery_voltage = 3.25
-        self.charged_battery_voltage = 3.6
+        self.imu.gyro_data_rate = Rate.RATE_104_HZ
+        self.imu.accelerometer_data_rate = Rate.RATE_104_HZ
+        self.imu.high_pass_filter = True
+        
+        self.bat.charge_current = self.bat.CHARGE_100MA
+
 
 
     @property
@@ -89,60 +101,128 @@ class IdleState(State):
 
     def enter(self, machine):
         State.enter(self, machine)
-        self.previous_msecs = time.monotonic()
-        if(self.charged == None):
-            if(self.bat.voltage >= self.discharged_battery_voltage):
-                self.charged = True
-            else:
-                self.charged = False
-        #self.previous_value = np.linalg.norm(self.imu.acceleration)
+        self.__previous_msecs = time.monotonic()
 
     def exit(self, machine):
         State.exit(self, machine)
-
-    def battery_monitor(self, bat_voltage):
-        if(bat_voltage > self.discharged_battery_voltage and bat_voltage < self.discharged_battery_voltage):
-            if(self.charged):
-                self.led_green.value = False
-                self.led_red.value = True
-            else:
-                self.led_green.value = True
-                self.led_red.value = False
-        elif(bat_voltage >= self.charged_battery_voltage):
-            self.led_green.value = False
-            self.led_red.value = True
-            self.charged = True
-        elif(bat_voltage <= self.discharged_battery_voltage):
-            self.led_green.value = True
-            self.led_red.value = False
-            self.charged = False
             
     def update(self, machine):
         #if switch.shaked:
         current_msecs = time.monotonic()
-        elapsed_time = (current_msecs - self.previous_msecs)
+        elapsed_time = (current_msecs - self.__previous_msecs)
         if(elapsed_time >= (1.0/self.loop_rate)):
             # Update timer
-            self.previous_msecs = current_msecs
+            self.__previous_msecs = current_msecs
             # Process data
-            try:
-                self.data_buffer[self.data_iter] = np.linalg.norm(self.imu.acceleration)
-                self.data_iter = (self.data_iter + 1)%(int(self.window_samples_size))
-                if(self.data_iter == int(self.window_samples_size)-1):
-                    #print(f"Voltage: {self.bat.voltage}")
-                    self.battery_monitor(self.bat.voltage)
-                    accel_difference = np.max([0,rms(self.data_buffer) - self.previous_value])
-                    self.previous_value = rms(self.data_buffer)
-                    #print((accel_difference,accel_difference))
-                    if(accel_difference > self.accel_diff_threshold):
-                        machine.go_to_state('shorting')
-            except:
-                print('Exception in idle or shorting state')
-                pass
+            self.data_buffer[self.data_iter] = np.linalg.norm(self.imu.acceleration)
+            #self.data_iter = (self.data_iter + 1)%(int(self.window_samples_size))
+            
+            if(self.data_iter == int(self.window_samples_size)-1):
+                self.data_iter = 0
+                accel_difference = np.max([0,rms(self.data_buffer) - self.previous_value])
+                self.previous_value = rms(self.data_buffer)
+                print((accel_difference,accel_difference))
+                if(accel_difference > self.accel_diff_threshold):
+                    machine.go_to_state('shorting')
+            else:
+                self.data_iter = self.data_iter + 1
+        
+class PausedState(IdleState):
+    def __init__(self):
+        pass
 
+    @property
+    def name(self):
+        return 'paused'
+
+    def enter(self, machine):
+        if(self.bat.voltage <= self.discharged_battery_voltage):
+            machine.go_to_state('idle-discharged')
+        else:
+            machine.go_to_state('idle-charged')
+
+    def exit(self, machine):
+        pass
+
+    def update(self, machine):
+        pass
+        
+class IdleDischargedState(IdleState):
+    def __init__(self):
+        self.__previous_msecs_discharged = time.monotonic()
+        
+    @property
+    def name(self):
+        return 'idle-discharged'
+
+    def enter(self, machine):
+        self.led_green.value = True
+        self.led_red.value = False
+        self.__previous_msecs_discharged = time.monotonic()
+        #pass
+
+    def exit(self, machine):
+        pass
+
+    def update(self, machine):
+        super().update(machine)
+        current_msecs = time.monotonic()
+        elapsed_time = (current_msecs - self.__previous_msecs_discharged)
+        if(elapsed_time >= (1.0/self.battery_loop_rate)):
+            # Update timer
+            self.__previous_msecs_discharged = current_msecs
+            # Process data
+            self.battery_data_buffer[self.battery_data_iter] = self.bat.voltage
+        
+            if(self.battery_data_iter == int(self.battery_window_samples_size)-1):
+                self.battery_data_iter = 0
+                bat_voltage = rms(self.battery_data_buffer)
+                print("Discharged battery: ", bat_voltage)
+                if(bat_voltage > self.charged_battery_voltage):
+                    machine.go_to_state('idle-charged')
+            else:
+                self.battery_data_iter = self.battery_data_iter + 1
+
+            
+class IdleChargedState(IdleState):
+    def __init__(self):
+        super().__init__()
+        self.__previous_msecs_charged = time.monotonic()
+        
+    @property
+    def name(self):
+        return 'idle-charged'
+
+    def enter(self, machine):
+        self.led_green.value = False
+        self.led_red.value = True
+        self.__previous_msecs_charged = time.monotonic()
+        
+    def exit(self, machine):
+        pass
+
+    def update(self, machine):
+        super().update(machine)
+        current_msecs = time.monotonic()
+        elapsed_time = (current_msecs - self.__previous_msecs_charged)
+        if(elapsed_time >= (1.0/self.battery_loop_rate)):
+            # Update timer
+            self.__previous_msecs_charged = current_msecs
+            # Process data
+            self.battery_data_buffer[self.battery_data_iter] = self.bat.voltage
+        
+            if(self.battery_data_iter == int(self.battery_window_samples_size)-1):
+                self.battery_data_iter = 0
+                bat_voltage = rms(self.battery_data_buffer)
+                print("Charged battery: ", bat_voltage)
+                if(bat_voltage < self.discharged_battery_voltage):
+                    machine.go_to_state('idle-discharged')
+            else:
+                self.battery_data_iter = self.battery_data_iter + 1
 class ShortingState(State):
 
     def __init__(self):
+        super().__init__()
         self.motor = digitalio.DigitalInOut(board.D0)
         self.motor.direction = digitalio.Direction.OUTPUT
         self.vibration_time = 8 # [s]
@@ -187,7 +267,7 @@ class ShortingState(State):
     def update(self, machine):
         if(time.monotonic()  - self.previous_msecs > self.vibration_time):
             self.motor.value = False
-            machine.go_to_state('idle')
+            machine.go_to_state(machine.get_previous_state())
 
 
 
@@ -195,8 +275,10 @@ class ShortingState(State):
 
 machine = StateMachine()
 machine.add_state(ShortingState())
-machine.add_state(IdleState())
-machine.go_to_state('idle')
+machine.add_state(IdleChargedState())
+machine.add_state(IdleDischargedState())
+machine.add_state(PausedState())
+machine.go_to_state('paused')
 
 while True:
     machine.update()
